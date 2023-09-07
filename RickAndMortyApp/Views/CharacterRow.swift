53292b623b9cf74072b76cf84d524c05de77e79f
Daniel Fernandez Merida <danielfernandezmerida@forch.es>
2023-09-07 18:31:10 +0200

    Cache images

--- RickAndMortyApp/Views/CharacterRow.swift
+++ RickAndMortyApp/Views/CharacterRow.swift
@@ -10,22 +10,38 @@ import SwiftUI
 struct CharacterRow : View {
     
     var character : RickAndMortyCharacter
+    @State private var cachedImage: UIImage? = nil
+
     
     var body: some View {
         HStack {
-            AsyncImage(url: URL(string: character.image)) { image in
-                
-                //Loaded image
-                image
-                    .resizable()
-
-            } placeholder: {
-                
-                //Default image if something didn't work
-                Image(systemName: "camera.circle.fill")
-                    .resizable()
+            
+            ZStack {
+                if let image = cachedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                } else {
+                    //Default image if something didn't work
+                    Image(systemName: "camera.circle.fill")
+                        .resizable()
+                }
             }
             .frame(width: 200, height: 200)
+            .onAppear {
+                if let url = URL(string: character.image) {
+                    descargarImagenDesdeURL(url) { (imagen) in
+                        if let imagen = imagen {
+                            // La imagen se ha descargado y almacenado en caché correctamente
+                            self.cachedImage = imagen
+                        } else {
+                            // No se pudo descargar la imagen
+                            print("Can't download the image")
+                        }
+                    }
+                }
+            }
             
             VStack(alignment: .leading) {
                 VStack(alignment: .leading) {
@@ -55,5 +71,38 @@ struct CharacterRow : View {
             }
             .padding()
         }
+    }
+    
+    
+    func descargarImagenDesdeURL(_ url: URL, completion: @escaping (UIImage?) -> Void) {
+        // Comprobar si la imagen está en la caché
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            if let image = UIImage(data: cachedResponse.data) {
+                completion(image)
+                return
+            }
+        }
+        
+        // Si la imagen no está en la caché, la descargamos
+        let session = URLSession.shared
+        let tarea = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error al descargar la imagen: \(error)")
+                completion(nil)
+                return
+            }
+            
+            if let data = data, let image = UIImage(data: data) {
+                // Almacenar la imagen en la caché
+                let cachedData = CachedURLResponse(response: response!, data: data)
+                URLCache.shared.storeCachedResponse(cachedData, for: URLRequest(url: url))
+                
+                completion(image)
+            } else {
+                completion(nil)
+            }
+        }
+        
+        tarea.resume()
     }
 }
